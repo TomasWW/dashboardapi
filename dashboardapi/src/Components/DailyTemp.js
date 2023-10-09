4aa660aac2f71489ff43066883888822c00091bf
TomasWW <tomas.woelflin@gmail.com>
2023-10-09 11:25:23 -0300

    Grafico utilza datos del json

--- dashboardapi/src/Components/DailyTemp.js
+++ dashboardapi/src/Components/DailyTemp.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from "react";
 import styled from "styled-components";
-import climaJson from "./climaApi"
+import climaApi from "./climaApi";
 import {
   BarChart,
   Bar,
@@ -12,96 +12,63 @@ import {
   ResponsiveContainer,
 } from "recharts";
 
-const data = [
-  {
-    name: "00hs",
-    Temperatura: 5,
-  },
-  {
-    name: "2hs",
-    Temperatura: 6,
-  },
-  {
-    name: "4hs",
-    Temperatura: 6,
-  },
-  {
-    name: "6hs",
-    Temperatura: 7,
-  },
-  {
-    name: "8hs",
-    Temperatura: 8,
-  },
-  {
-    name: "10hs",
-    Temperatura: 14,
-  },
-  {
-    name: "12hs",
-    Temperatura: 16,
-  },
-  {
-    name: "14hs",
-    Temperatura: 20,
-  },
-  {
-    name: "16hs",
-    Temperatura: 22,
-  },
-  {
-    name: "18hs",
-    Temperatura: 14,
-  },
-  {
-    name: "20hs",
-    Temperatura: 10,
-  },
-  {
-    name: "22hs",
-    Temperatura: 6,
-  },
-  {
-    name: "24hs",
-    Temperatura: 6,
-  },
-];
-
 export default class DailyTemp extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [], // Inicializamos data como un array vacío
+    };
+  }
+
+  componentDidMount() {
+    // Extraemos los datos de temperaturas horarias de climaApi dentro de componentDidMount
+    const hourlyTemperatures = climaApi.hourly.temperature_2m;
+
+    // Creamos un nuevo array de objetos que contiene el horario y la temperatura
+    const newData = climaApi.hourly.time.map((time, index) => ({
+      name: this.formatTime(time),
+      Temperatura: hourlyTemperatures[index],
+    }));
+
+    // Actualizamos el estado con los nuevos datos
+    this.setState({ data: newData });
+  }
+  formatTime(time) {
+    const date = new Date(time);
+    return `${date.getHours()}hs`;
+  }
   render() {
     return (
-   
-        <ResponsiveContainer width="100%" height="100%">
-          <BarChart
-            width={5}
-            height={3}
-            data={data}
-            margin={{
-              top: 5,
-              right: 30,
-              left: 20,
-              bottom: 5,
-            }}
-            barCategoryGap="10%"
-            barGap="5%"
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Bar
-              dataKey="Temperatura"
-              fill="#c2948a"
-              barSize={10}
-              radius={[15, 15, 0, 0]}
-            />{" "}
-            <text x="50%" y="5%" textAnchor="middle" dominantBaseline="middle">
-              Temperatura Horaria
-            </text>
-          </BarChart>
-        </ResponsiveContainer>
-      
+      <ResponsiveContainer width="100%" height="100%">
+        <BarChart
+          width={500}
+          height={300}
+          data={this.state.data} // Usamos this.state.data en lugar de data
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+          barCategoryGap="10%"
+          barGap="5%"
+        >
+          <CartesianGrid strokeDasharray=" 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar
+            dataKey="Temperatura"
+            fill="#c2948a"
+            barSize={10}
+            radius={[15, 15, 0, 0]}
+          />
+          <text x="50%" y="5%" textAnchor="middle" dominantBaseline="middle">
+            Temperatura Horaria
+          </text>
+        </BarChart>
+      </ResponsiveContainer>
     );
   }
 }
